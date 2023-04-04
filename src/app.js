57b2c62c97207dcc6735ab890896abcdc816bf77
dzagar88 <dzagarit@gmail.com>
2023-04-04 16:13:51 +0000

    complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").innerHTML = randNumbers();
+  document.querySelector(".card").classList.add(randSuits());
+};
+
+const randNumbers = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  let number = Math.floor(Math.random() * numbers.length);
+  console.log(numbers[number]);
+  return numbers[number];
+};
+
+const randSuits = () => {
+  let suits = ["club", "heart", "spade", "diamonds"];
+  let suit = Math.floor(Math.random() * suits.length);
+  console.log(suits[suit]);
+  return suits[suit];
 };
